8457f5f0e509c6ed718516f969f40e550edfbfbc
Jabo <hjbno.1@qq.com>
2018-11-14 18:15:40 +0800

    init vue3.0
    vue-cli3.0

--- src/main.js
+++ src/main.js
@@ -2,11 +2,51 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import axios from "axios";
+import ElemnentUI from "element-ui";
+import "./element-variables.scss";
+// import config from "../config";
 
-Vue.config.productionTip = false;
+Vue.use(ElemnentUI);
 
 new Vue({
   router,
   store,
+  created() {
+    let that = this;
+    let loading;
+    // 添加请求拦截器
+    axios.interceptors.request.use(
+      function(config) {
+        // 在发送请求之前做什么
+        if (config.loading !== false) {
+          loading = that.$loading({
+            fullscreen: true,
+            background: "rgba(255,255,255,0.25)"
+          });
+        }
+        return config;
+      },
+      function(error) {
+        // 请求错误做什么
+        loading && loading.close();
+        return Promise.reject(error);
+      }
+    );
+
+    // 添加响应拦截器
+    axios.interceptors.response.use(function(response) {
+      let resConfig = response.config;
+      // 对响应数据做点什么
+      if (resConfig) {
+        that.$message({
+          showClose: true,
+          message: resConfig,
+          type: "success"
+        });
+      }
+      loading && loading.close();
+    });
+  },
   render: h => h(App)
 }).$mount("#app");
---
+++ src/views/frame/index.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="index">
+		<header class="index-header">
+			
+		</header>
+		<aside class="index-aside">
+			
+		</aside>
+		<main class="index-main">
+			
+			
+		</main>
+	</div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
